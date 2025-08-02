4fa7204b70a00b69ec7f97ff05f9438fb3133982
Andrey Perez <andreyperez109@gmail.com>
2025-08-02 09:33:34 -0600

    Se agrgaron cosas

--- js/carousel.js
+++ js/carousel.js
@@ -9,9 +9,6 @@ $(document).ready(function () {
         html += `
         <div class="carousel-item ${index === 0 ? 'active' : ''}">
           <img src="${item.imagen}" class="d-block w-100 carousel-img" alt="${item.nombre}">
-          <div class="carousel-caption">
-            <button class="btn carousel-Boton text-light p-2 p-md-3 fs-6 fs-md-5">${item.nombre}</button>
-          </div>
         </div>
       `;
       });
