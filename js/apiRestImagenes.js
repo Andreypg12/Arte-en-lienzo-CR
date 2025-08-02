6e7e9ad666c476ff922227f7c57fe7ce99d8874d
Andrey Perez <andreyperez109@gmail.com>
2025-08-02 01:49:37 -0600

    se agragaron cosas

--- js/apiRestImagenes.js
+++ js/apiRestImagenes.js
@@ -1,10 +1,14 @@
-fetch("../json/imagenes.json")
-  .then(response => response.json())
-  .then(data => {
-    const banner = data.find(item => item.nombre === "Banner");
-    if (banner) {
-      document.querySelectorAll('[name="banner"]').forEach(element => {
-        element.innerHTML = `<img src="${banner.imagen}" alt="${banner.descripcion}" />`;
-      });
-    }
-  });
+$(document).ready(function () {
+
+  fetch("../json/imagenes.json")
+    .then(response => response.json())
+    .then(data => {
+      const banner = data.find(item => item.nombre === "Banner");
+      if (banner) {
+        document.querySelectorAll('[name="banner"]').forEach(element => {
+          element.innerHTML = `<img src="${banner.imagen}" alt="${banner.descripcion}" />`;
+        });
+      }
+    });
+
+});
