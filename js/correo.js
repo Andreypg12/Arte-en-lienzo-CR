29d1d0e77eeb6cf84bf049f2e1a2e4735544aa51
AleSC203 <ale1010192@gmail.com>
2025-08-05 08:55:20 -0600

    Funcion enviar correo

---
+++ js/correo.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  const btn = document.getElementById("button");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // evita recarga
+
+    btn.value = "Enviando...";
+
+    const serviceID = "default_service";
+    const templateID = "template_xhqmv1r";
+
+    emailjs.sendForm(serviceID, templateID, form)
+      .then(() => {
+        btn.value = "Enviar";
+        alert("Mensaje enviado correctamente!");
+        form.reset(); 
+      }, (err) => {
+        btn.value = "Enviar";
+        alert("Error al enviar el mensaje:\n" + JSON.stringify(err));
+      });
+  });
+});
