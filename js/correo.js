31d1d91375caef589302169479425d12a9b3c5ad
AleSC203 <ale1010192@gmail.com>
2025-08-19 10:45:08 -0600

    pequeños cambios y comentarios

--- js/correo.js
+++ js/correo.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+
   const form = document.getElementById("form");
   const btn = document.getElementById("button");
 
   form.addEventListener("submit", function (e) {
-    e.preventDefault(); // evita recarga
+    e.preventDefault(); // evita recarga porque sino no se envía el mensaje
 
     btn.value = "Enviando...";
 
     const serviceID = "default_service";
     const templateID = "template_xhqmv1r";
 
-    emailjs.sendForm(serviceID, templateID, form)
-      .then(() => {
+    emailjs.sendForm(serviceID, templateID, form).then(() => {
+
         btn.value = "Enviar";
         alert("Mensaje enviado correctamente!");
-        form.reset(); 
-      }, (err) => {
-        btn.value = "Enviar";
-        alert("Error al enviar el mensaje:\n" + JSON.stringify(err));
-      });
+        form.reset();
+
+      },
+        (err) => {
+          btn.value = "Enviar";
+          alert("Error al enviar el mensaje:\n" + JSON.stringify(err));
+
+        });
+
   });
+
+
 });
